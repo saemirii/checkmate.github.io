70af4bd1e66355e6d12eb845268a2d925df41c4c
carlosrrdev <rr.carlos.dev@gmail.com>
2024-05-19 22:37:06 -0500

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "selector",
+  content: ["./index.html", "./js/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
